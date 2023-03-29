e0b7070689e4e8e19b65902ffc7dc03ce135ee36
mohammed gharab <121175508+MOHAMMED75G@users.noreply.github.com>
2023-03-29 18:14:20 +0000

    Add files via upload

---
+++ Hero.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import phoneImg from './images/phone.svg';
+import { useGlobalContext } from './context';
+
+const Hero = () => {
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className='hero' onMouseOver={closeSubmenu}>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>
+            Payment for RoboCup<br />
+              Welcome
+          </h1>
+          <p>
+            cette site permet de verifier votre payment , s'il vous plait insert le photo
+          </p>
+          <button className='btn'>Start now</button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} className='phone-img' alt='phone' />
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
